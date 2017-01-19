941268009a3c74c5dd5864aaece0f30f3cddb20c
Ben Mango <ben@benmango.co.uk>
2017-01-19 11:33:01 +0000

    Site complete prior to final comments

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -2,7 +2,7 @@
  * Created by Ben on 13/01/2017.
  */
 $(document).ready(function() {
-    $('.navbar a.dropdown-toggle').on('mouseover', function(e) {
+    $('.navbar a.dropdown-toggle').on('click', function(e) {
         var $el = $(this);
         var $parent = $(this).offsetParent(".dropdown-menu");
         $(this).parent("li").toggleClass('open');
@@ -15,4 +15,33 @@ $(document).ready(function() {
 
         //return false;
     });
+
+    //History Accordion
+    //Add Inactive Class To All Accordion Headers
+    $('.accordion-header').toggleClass('inactive-header');
+
+    //Set The Accordion Content Width
+    var contentwidth = $('.accordion-header').width();
+    $('.accordion-content').css({'width' : contentwidth });
+
+    //Open The First Accordion Section When Page Loads
+    //$('.accordion-header').first().toggleClass('active-header').toggleClass('inactive-header');
+    //$('.accordion-content').first().slideDown().toggleClass('open-content');
+
+    // The Accordion Effect
+    $('.accordion-header').click(function () {
+        if($(this).is('.inactive-header')) {
+            $('.active-header').toggleClass('active-header').toggleClass('inactive-header').next().slideToggle().toggleClass('open-content');
+            $(this).toggleClass('active-header').toggleClass('inactive-header');
+            $(this).next().slideToggle().toggleClass('open-content');
+        }
+
+        else {
+            $(this).toggleClass('active-header').toggleClass('inactive-header');
+            $(this).next().slideToggle().toggleClass('open-content');
+        }
+    });
+
+//end history accordion
+
 });
